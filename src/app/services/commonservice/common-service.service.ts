c681c0b8df875bba2fc33cdcb8882fb61a921811
Vijay kumar~ <vijay.kumar40@ibm.com>
2024-02-24 15:44:50 +0530

    updates

--- src/app/services/commonservice/common-service.service.ts
+++ src/app/services/commonservice/common-service.service.ts
@@ -1,4 +1,3 @@
-// import { isPlatformBrowser } from '@angular/common';
 import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import Swal from "sweetalert2";
@@ -6,36 +5,24 @@ import Swal from "sweetalert2";
   providedIn: 'root'
 })
 export class CommonService {
-  // public isBrowser: boolean = false;
   public renderer: Renderer2;
   public showMenu: boolean = false;
   private modalReference: any;
   constructor(
-    // @Inject(PLATFORM_ID) platformId: Object,
     public rendererFactory: RendererFactory2,
     private modalService: NgbModal,
     config: NgbModalConfig,
   ) {
-    // this.isBrowser = isPlatformBrowser(platformId);
     this.renderer = rendererFactory.createRenderer(null, null);
     config.backdrop = "static";
     config.keyboard = false;
   }
-
-
   public OpenPopup(content: any, selclass = "My_Popup", size = 'lg') {
     this.modalReference = this.modalService.open(content, { centered: true, windowClass: selclass, size });
   }
   public CloseModal() {
     this.modalReference ? this.modalReference.close() : "";
   }
-
-  // public toggleOverflow() {
-  //   if (this.isBrowser) {
-  //     (this.showMenu) ? this.renderer.addClass(window.document.body, 'overflowx') : this.renderer.removeClass(window.document.body, 'overflowx');
-  //     (this.showMenu) ? this.renderer.addClass(window.document.documentElement, 'overflowx') : this.renderer.removeClass(window.document.documentElement, 'overflowx');
-  //   }
-  // }
   // SWEET ALERT
   public SwalSuccess(msg: string, heading = "Success!") {
     Swal.fire({
@@ -65,7 +52,6 @@ export class CommonService {
   public SwalClose() {
     Swal.close()
   }
-
   public SwalSuccessAutoclose(msg: string, timer = 2000, heading = "Success!") {
     Swal.fire({
       title: heading,
@@ -76,5 +62,4 @@ export class CommonService {
       timerProgressBar: true,
     });
   }
-
 }
